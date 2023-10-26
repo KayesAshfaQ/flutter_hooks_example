c8ccb33c47f5ee64a2d4884c6489b71fa576c076
kayesAshfaq <asfak7074@gmail.com>
2023-10-26 09:54:38 +0600

    animation_controller_hook added

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,78 +1,74 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends HookWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+  // @override
+  // void initState() {
+  //   _scrollController = ScrollController();
+  //   _hideFabAnimController = AnimationController(
+  //     vsync: this,
+  //     duration: const Duration(milliseconds: 300),
+  //     value: 1,
+  //   );
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  late ScrollController _scrollController;
-  late AnimationController _hideFabAnimController;
+  //   _scrollController.addListener(() {
+  //     switch (_scrollController.position.userScrollDirection) {
+  //       // Scrolling up - forward the animation (value goes to 1)
+  //       case ScrollDirection.forward:
+  //         _hideFabAnimController.forward();
+  //         break;
+  //       // Scrolling down - reverse the animation (value goes to 0)
+  //       case ScrollDirection.reverse:
+  //         _hideFabAnimController.reverse();
+  //         break;
+  //       // Idle - keep FAB visibility unchanged
+  //       case ScrollDirection.idle:
+  //         break;
+  //     }
+  //   });
 
-  @override
-  void initState() {
-    _scrollController = ScrollController();
-    _hideFabAnimController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-      value: 1,
-    );
-
-    _scrollController.addListener(() {
-      switch (_scrollController.position.userScrollDirection) {
-        // Scrolling up - forward the animation (value goes to 1)
-        case ScrollDirection.forward:
-          _hideFabAnimController.forward();
-          break;
-        // Scrolling down - reverse the animation (value goes to 0)
-        case ScrollDirection.reverse:
-          _hideFabAnimController.reverse();
-          break;
-        // Idle - keep FAB visibility unchanged
-        case ScrollDirection.idle:
-          break;
-      }
-    });
-
-    super.initState();
-  }
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
+    final _hideFabAnimController = useAnimationController(
+      duration: const Duration(milliseconds: 300),
+      initialValue: 1,
+    );
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Flutter Hooks Home Page'),
       ),
       body: ListView(
-      controller: _scrollController,
-      children: <Widget>[
-        for (int i = 0; i < 5; i++)
-          const Card(child: FittedBox(child: FlutterLogo())),
-      ],
-    ),
+        //controller: _scrollController,
+        children: <Widget>[
+          for (int i = 0; i < 5; i++)
+            const Card(child: FittedBox(child: FlutterLogo())),
+        ],
+      ),
       floatingActionButton: FadeTransition(
-      opacity: _hideFabAnimController,
-      child: ScaleTransition(
-        scale: _hideFabAnimController,
-        child: FloatingActionButton.extended(
-          label: const Text('Useless Floating Action Button'),
-          onPressed: () {},
+        opacity: _hideFabAnimController,
+        child: ScaleTransition(
+          scale: _hideFabAnimController,
+          child: FloatingActionButton.extended(
+            label: const Text('Useless Floating Action Button'),
+            onPressed: () {},
+          ),
         ),
       ),
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    _hideFabAnimController.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   _scrollController.dispose();
+  //   _hideFabAnimController.dispose();
+  //   super.dispose();
+  // }
 }
